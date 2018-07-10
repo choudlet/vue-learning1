52134b8f22b0bb569489e6a26e48a2970940ee6e
Christopher Houdlette <christopher@choozle.com>
2018-07-10 09:37:12 -0600

    adding in conditional styles

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,14 @@
 </template>
 
 <script>
-import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
+import HomePage from "./home/HomePage.vue";
+import RobotBuilder from "./build/RobotBuilder.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    RobotBuilder,
-  },
+    RobotBuilder
+  }
 };
 </script>
 
@@ -31,9 +31,11 @@ body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
 }
+</style>
 
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 
 main {
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
         <div class="top-row">
-            <div class="top part">
+            <div class="top part" :class="[saleBorderClass]">
                 <div class="robot-name">
                     {{selectedRobot.head.title}}
                     <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -58,7 +58,7 @@
 </template>
 
 <script>
-import availableParts from '../data/parts';
+import availableParts from "../data/parts";
 
 function getPreviousValidIndex(index, length) {
   const depreciatedIndex = index - 1;
@@ -71,199 +71,252 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  name: 'RobotBuilder',
+  name: "RobotBuilder",
   data() {
     return {
-      availableParts, cart: [], selectedHeadIndex: 0, selectedLeftArmIndex: 0, selectedTorsoIndex: 0, selectedRightArmIndex: 0, selectedBaseIndex: 0,
+      availableParts,
+      cart: [],
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedBaseIndex: 0
     };
   },
   computed: {
+    saleBorderClass() {
+      return selectedRobot.head.onSale ? 'sale-border' : '';
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
         leftArm: availableParts.arms[this.selectedLeftArmIndex],
         torso: availableParts.torsos[this.selectedTorsoIndex],
         rightArm: availableParts.arms[this.selectedRightArmIndex],
-        base: availableParts.bases[this.selectedBaseIndex],
+        base: availableParts.bases[this.selectedBaseIndex]
       };
     },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale ? '3px solid red' :'3px solid #aaa'
+      }
+    }
   },
   methods: {
     selectNextHead() {
-      this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex, availableParts.heads.length);
+      this.selectedHeadIndex = getNextValidIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length
+      );
     },
     selectPreviousHead() {
-      this.selectedHeadIndex = getPreviousValidIndex(this.selectedHeadIndex, availableParts.heads.length);
+      this.selectedHeadIndex = getPreviousValidIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length
+      );
     },
     selectNextLeftArm() {
-      this.selectedLeftArmIndex = getNextValidIndex(this.selectedLeftArmIndex, availableParts.arms.length);
+      this.selectedLeftArmIndex = getNextValidIndex(
+        this.selectedLeftArmIndex,
+        availableParts.arms.length
+      );
     },
     selectPreviousLeftArm() {
-      this.selectedLeftArmIndex = getPreviousValidIndex(this.selectedLeftArmIndex, availableParts.arms.length);
+      this.selectedLeftArmIndex = getPreviousValidIndex(
+        this.selectedLeftArmIndex,
+        availableParts.arms.length
+      );
     },
     selectNextTorso() {
-      this.selectedTorsoIndex = getNextValidIndex(this.selectedTorsoIndex, availableParts.torsos.length);
+      this.selectedTorsoIndex = getNextValidIndex(
+        this.selectedTorsoIndex,
+        availableParts.torsos.length
+      );
     },
     selectPreviousTorso() {
-      this.selectedTorsoIndex = getPreviousValidIndex(this.selectedTorsoIndex, availableParts.torsos.length);
+      this.selectedTorsoIndex = getPreviousValidIndex(
+        this.selectedTorsoIndex,
+        availableParts.torsos.length
+      );
     },
     selectNextRightArm() {
-      this.selectedRightArmIndex = getNextValidIndex(this.selectedRightArmIndex, availableParts.arms.length);
+      this.selectedRightArmIndex = getNextValidIndex(
+        this.selectedRightArmIndex,
+        availableParts.arms.length
+      );
     },
     selectPreviousRightArm() {
-      this.selectedRightArmIndex = getPreviousValidIndex(this.selectedRightArmIndex, availableParts.arms.length);
+      this.selectedRightArmIndex = getPreviousValidIndex(
+        this.selectedRightArmIndex,
+        availableParts.arms.length
+      );
     },
     selectNextBase() {
-      this.selectedBaseIndex = getNextValidIndex(this.selectedBaseIndex, availableParts.bases.length);
+      this.selectedBaseIndex = getNextValidIndex(
+        this.selectedBaseIndex,
+        availableParts.bases.length
+      );
     },
     selectPreviousBase() {
-      this.selectedBaseIndex = getPreviousValidIndex(this.selectedBaseIndex, availableParts.arms.length);
+      this.selectedBaseIndex = getPreviousValidIndex(
+        this.selectedBaseIndex,
+        availableParts.arms.length
+      );
     },
     addToCart() {
       const robot = this.selectedRobot;
-      const cost = robot.head.cost + robot.rightArm.cost + robot.torso.cost + robot.leftArm.cost + robot.base.cost;
+      const cost =
+        robot.head.cost +
+        robot.rightArm.cost +
+        robot.torso.cost +
+        robot.leftArm.cost +
+        robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style>
 .part {
-    position: relative;
-    width: 165px;
-    height: 165px;
-    border: 3px solid #aaa;
+  position: relative;
+  width: 165px;
+  height: 165px;
+  border: 3px solid #aaa;
 }
 
 .part img {
-    width: 165px;
+  width: 165px;
 }
 
 .top-row {
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 }
 
 .middle-row {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .bottom-row {
-    display: flex;
-    justify-content: space-around;
-    border-top: none;
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
 }
 
 .head {
-    border-bottom: none;
+  border-bottom: none;
 }
 
 .left {
-    border-right: none;
+  border-right: none;
 }
 
 .right {
-    border-left: none;
+  border-left: none;
 }
 
 .left img {
-    transform: rotate(-90deg);
+  transform: rotate(-90deg);
 }
 
 .right img {
-    transform: rotate(90deg);
+  transform: rotate(90deg);
 }
 
 .bottom {
-    border-top: none;
+  border-top: none;
 }
 
 .prev-selector {
-    position: absolute;
-    z-index: 1;
-    top: -3px;
-    left: -28px;
-    width: 25px;
-    height: 171px;
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
 }
 
 .next-selector {
-    position: absolute;
-    z-index: 1;
-    top: -3px;
-    right: -28px;
-    width: 25px;
-    height: 171px;
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
 }
 
 .center .prev-selector,
 .center .next-selector {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 
 .left .prev-selector {
-    top: -28px;
-    left: -3px;
-    width: 144px;
-    height: 25px;
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
 }
 
 .left .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: -3px;
-    width: 144px;
-    height: 25px;
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
 }
 
 .right .prev-selector {
-    top: -28px;
-    left: 24px;
-    width: 144px;
-    height: 25px;
+  top: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
 }
 
 .right .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: 24px;
-    width: 144px;
-    height: 25px;
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
 }
 
 .right .next-selector {
-    right: -3px;
+  right: -3px;
 }
 
 .robot-name {
-    position: absolute;
-    top: -25px;
-    text-align: center;
-    width: 100%;
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
 }
 
 .sale {
-    color: red;
+  color: red;
 }
 
 .content {
-    position: relative;
+  position: relative;
 }
 
 .add-to-cart {
-    position: absolute;
-    right: 30px;
-    width: 220px;
-    padding: 3px;
-    font-size: 16px;
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
 }
 
 td,
 th {
-    text-align: left;
-    padding: 5px;
-    padding-right: 20px;
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+
+.sale-border {
+  border: 3px solid red;
 }
 </style>
