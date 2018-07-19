0b38dabdc55f0c4ec642d268f4ae79dd192cce57
Christopher Houdlette <christopher@choozle.com>
2018-07-18 21:34:10 -0600

    learned about passing data down and back up to parent components through emitting events

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,22 +1,38 @@
 <template>
     <div class="content">
+        <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
         <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
+      </div>
+    </div>
+        
         <div class="top-row">
             <div  :class="[saleBorderClass, 'top', 'part']">
                 <!-- <div class="robot-name">
                     {{selectedRobot.head.title}}
                     <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
                 </div> -->
-                <PartSelector />
+                <PartSelector @partSelected="part => selectedRobot.head=part" position="top" :parts="availableParts.heads" />
             </div>
         </div>
         <div class="middle-row">
-          <PartSelector />
-          <PartSelector />
-          <PartSelector />
+          <PartSelector @partSelected="part => selectedRobot.leftArm=part" position="left" :parts="availableParts.arms" />
+          <PartSelector @partSelected="part => selectedRobot.torso=part" position="center" :parts="availableParts.torsos" />
+          <PartSelector @partSelected="part => selectedRobot.rightArm=part" position="right" :parts="availableParts.arms" />
         </div>
         <div class="bottom-row">
-            <PartSelector />
+            <PartSelector @partSelected="part => selectedRobot.base=part" position="bottom" :parts="availableParts.bases" />
         </div>
         <div>
             <h1>Cart</h1>
@@ -225,8 +241,7 @@ export default {
 
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
 }
@@ -240,5 +255,27 @@ th {
 
 .sale-border {
   border: 3px solid red;
+}
+
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
